a6f69926b089faed8cca4bf5e61a2450ac3c02e8
jlinbar0906 <jlinbar0906@g.educaand.es>
2023-11-03 13:18:32 +0100

    Ejercicio 1 terminado

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        num = sc.nextInt();
+        if(num%2==0){
+            for (int i=10;i<num;i=i+5){
+                System.out.println(i);
+            }
+        }else{
+            for (int i=10;i<num;i=i+3){
+                System.out.println(i);
+            }
+        }
+
+
+    }
 }
+
